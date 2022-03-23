088a90f38a00e8735af85e53cd368ae50c99338b
hafidh2001 <hafidhfauzan2@gmail.com>
2022-03-24 01:06:23 +0700

    crud-getAllData-OK

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,73 +1,38 @@
 // import module with object literal {}
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Todolist = () => {
+  // destructuring array | initial value = empty array
+  const [lists, setList] = useState([]);
+
+  // use effect componentDidMount | initial value = empty array
+  useEffect(() => {
+    getList();
+  }, []); // empty array in useEffect for stop looping run function getList
+
+  // asynchronous arrow function expression get-allData
+  const getList = async () => {
+    const response = await axios.get("http://localhost:5000/todoApps");
+    console.log(response.data);
+    setList(response.data);
+  };
+
   return (
     <div className="component-home flex flex-col">
-      <div className="card-note">
-        <span className="text-note">
-          Belajar dasar JavaScript, Belajar Dasar ReactJS, Belajar Dasar
-          Tailwindcss, Belajar Dasar NodeJS
-        </span>
-        <div className="button m-1 basis-1/5 flex justify-center">
-          <button className=" w-7 h-7 m-1 mr-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">✏️</span>
-          </button>
-          <button className="w-7 h-7 m-1 ml-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">❌</span>
-          </button>
+      {lists.map((list) => (
+        <div key={list.id} className="card-note">
+          <span className="text-note">{list.text}</span>
+          <div className="button m-1 basis-1/5 flex justify-center">
+            <button className=" w-7 h-7 m-1 mr-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
+              <span className="m-auto text-sm group-hover:scale-105">✏️</span>
+            </button>
+            <button className="w-7 h-7 m-1 ml-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
+              <span className="m-auto text-sm group-hover:scale-105">❌</span>
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="card-note">
-        <span className="text-note">Belajar membuat frontend dan backend</span>
-        <div className="button m-1 basis-1/5 flex justify-center">
-          <button className=" w-7 h-7 m-1 mr-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">✏️</span>
-          </button>
-          <button className="w-7 h-7 m-1 ml-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">❌</span>
-          </button>
-        </div>
-      </div>
-      <div className="card-note">
-        <span className="text-note">
-          Integrasi database dari MySQL ke database PostgreSQL
-        </span>
-        <div className="button m-1 basis-1/5 flex justify-center">
-          <button className=" w-7 h-7 m-1 mr-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">✏️</span>
-          </button>
-          <button className="w-7 h-7 m-1 ml-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">❌</span>
-          </button>
-        </div>
-      </div>
-      <div className="card-note">
-        <span className="text-note">
-          Belajar menggunakan framework Andromedia
-        </span>
-        <div className="button m-1 basis-1/5 flex justify-center">
-          <button className=" w-7 h-7 m-1 mr-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">✏️</span>
-          </button>
-          <button className="w-7 h-7 m-1 ml-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">❌</span>
-          </button>
-        </div>
-      </div>
-      <div className="card-note">
-        <span className="text-note">
-          Integrasi database PostgreSQL ke Prisma.IO
-        </span>
-        <div className="button m-1 basis-1/5 flex justify-center">
-          <button className=" w-7 h-7 m-1 mr-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">✏️</span>
-          </button>
-          <button className="w-7 h-7 m-1 ml-[0.5px] p-0 bg-slate-800 rounded-md cursor-pointer hover:bg-slate-900 hover:scale-105 group">
-            <span className="m-auto text-sm group-hover:scale-105">❌</span>
-          </button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
